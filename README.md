3a2f61cf4b159cf632721913d8687b3497d620b5
Kawaharasouta <kawahara6514@gmail.com>
2019-07-13 14:56:14 +0900

    del _build

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# wiki
+# jr-nttcom
+
+setup env on ubuntu
+```
+sudo pip3 install sphinx sphinx_rtd_theme
+sudo apt install -y texlive-fonts-recommended \
+		texlive-latex-recommended texlive-latex-extra \
+		texlive-lang-japanese latexmk texlive-latex-base python3-sphinx
+git clone <this-repo> && cd $_
+make html
+open _build/html/index.html
+```
+
+
+
---
+++ conf.py
@@ -0,0 +1,57 @@
+# -*- coding: utf-8 -*-
+
+# -- Project information -----------------------------------------------------
+project = u'KamueeとLinuxのL3転送性能の比較'
+copyright = '2018, Hosei University , NTT Communications'
+author = 'Souta Kawahara, Hiroki Shirokura'
+version = ''
+release = '0.0.0'
+
+# -- General configuration ---------------------------------------------------
+extensions = [ 'sphinx.ext.todo' ]
+templates_path = ['_templates']
+source_suffix = '.rst'
+master_doc = 'index'
+language = 'ja'
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
+pygments_style = None
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for HTMLHelp output ---------------------------------------------
+htmlhelp_basename = 'jr-nttcom'
+
+# -- Options for LaTeX output ------------------------------------------------
+latex_elements = {
+    'extraclassoptions': 'openany',
+    # The paper size ('letterpaper' or 'a4paper').
+    # 'papersize': 'letterpaper',
+    # The font size ('10pt', '11pt' or '12pt').
+    # 'pointsize': '10pt',
+    # Additional stuff for the LaTeX preamble.
+    # 'preamble': '',
+    # Latex figure (float) alignment
+    # 'figure_align': 'htbp',
+}
+latex_docclas = {'manual': 'jsbook'}
+latex_documents = [
+    (master_doc, 'report.tex', project,
+     'Souta Kawahara, Hiroki Shirokura', 'manual'),
+]
+
+# -- Options for manual page output ------------------------------------------
+man_pages = [
+    (master_doc, 'jr-nttcom', project,
+     [author], 1)
+]
+
+# -- Options for Texinfo output ----------------------------------------------
+texinfo_documents = [
+    (master_doc, 'report', project,
+     author, 'report', 'One line description of project.',
+     'Miscellaneous'),
+]
+
+# -- Options for Epub output -------------------------------------------------
+epub_title = project
+epub_exclude_files = ['search.html']
