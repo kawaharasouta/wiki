bfdb98e6cc6167ce33441c4770ff7ebef1e58f96
Kawaharasouta <kawahara6514@gmail.com>
2019-07-13 16:07:09 +0900

    fix conf.py

--- conf.py
+++ conf.py
@@ -19,39 +19,6 @@ html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
 
 # -- Options for HTMLHelp output ---------------------------------------------
-htmlhelp_basename = 'jr-nttcom'
+htmlhelp_basename = 'wiki of khwarizmi'
 
-# -- Options for LaTeX output ------------------------------------------------
-latex_elements = {
-    'extraclassoptions': 'openany',
-    # The paper size ('letterpaper' or 'a4paper').
-    # 'papersize': 'letterpaper',
-    # The font size ('10pt', '11pt' or '12pt').
-    # 'pointsize': '10pt',
-    # Additional stuff for the LaTeX preamble.
-    # 'preamble': '',
-    # Latex figure (float) alignment
-    # 'figure_align': 'htbp',
-}
-latex_docclas = {'manual': 'jsbook'}
-latex_documents = [
-    (master_doc, 'report.tex', project,
-     'Souta Kawahara, Hiroki Shirokura', 'manual'),
-]
 
-# -- Options for manual page output ------------------------------------------
-man_pages = [
-    (master_doc, 'jr-nttcom', project,
-     [author], 1)
-]
-
-# -- Options for Texinfo output ----------------------------------------------
-texinfo_documents = [
-    (master_doc, 'report', project,
-     author, 'report', 'One line description of project.',
-     'Miscellaneous'),
-]
-
-# -- Options for Epub output -------------------------------------------------
-epub_title = project
-epub_exclude_files = ['search.html']
